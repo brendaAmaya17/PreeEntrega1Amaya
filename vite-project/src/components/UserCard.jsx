c4c4cb0f6e6a0b9e46de5e58630236dc51ea8028
brenda amaya <amayabrenda2017@gmail.com>
2024-03-25 21:23:29 -0300

    primeros pasos

---
+++ vite-project/src/components/UserCard.jsx
@@ -0,0 +1,36 @@
+import { Button } from "antd";
+import { useState } from "react";
+function UserCard(props) {
+
+    const elResultado = useState(1)
+    const miEstado = elResultado[0] //1
+    const setMiEstado = elResultado[1] //function
+
+    /* console.log(elResultado) */
+
+    const [counter,setCounter] = useState(0)
+    //let counter = 0
+
+    const handleClick = () => {
+        //counter++
+        //counter += 1
+        //counter = counter + 1
+        const nuevoValor = counter + 1
+        setCounter(nuevoValor)
+    }
+
+    return (
+        <div className="p-4 transition rounded-md shadow-md user-card hover:scale-105">
+            <img className="w-full rounded-md" src="https://picsum.photos/400" alt="card image" />
+            <h2 className="my-2 font-bold">{props.name}</h2>
+            <Button>ver mas</Button>
+            {/* <button onClick={handleClick}>
+                like
+                <span>{counter}</span>
+            </button> */}
+        </div>
+    )
+}
+
+
+export default UserCard;
